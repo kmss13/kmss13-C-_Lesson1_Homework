cc53056c7272f74af23dea53d43ccb66265a17f9
Viking <kmss13@mail.ru>
2023-06-12 12:11:14 +0400

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+string inputString = Console.ReadLine();
+int number = Convert.ToInt32(inputString);
+int count = 1;
+
+Console.WriteLine();// Пустая строка для разграничения введенного числа и вывода чисел
+
+while (count < number) 
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);  
+    }
+
+count++;
+}
