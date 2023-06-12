2468c1a69115e91ea8281cdc09e3eddd8c0dae9d
Viking <kmss13@mail.ru>
2023-06-12 11:59:17 +0400

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string inputString = Console.ReadLine();
+int number = Convert.ToInt32(inputString);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Введенное число чётное");
+}
+else
+{
+    Console.WriteLine("Введенное число нечётное");
+}
+
