5b4dab9a15c5d7645ad560d73fb45ae9b91480b5
Viking <kmss13@mail.ru>
2023-06-12 11:50:30 +0400

    add task1 and task2

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+string inputString1 = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string inputString2 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(inputString1);
+int number2 = Convert.ToInt32(inputString2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} больше числа {number1}");
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число");
+string inputString1 = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string inputString2 = Console.ReadLine();
+
+Console.WriteLine("Введите третье число");
+string inputString3 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(inputString1);
+int number2 = Convert.ToInt32(inputString2);
+int number3 = Convert.ToInt32(inputString3);
+int max;
+
+if (number2 > number1)
+{
+    max = number2;
+}
+else
+{
+    max = number1;
+}
+if (number3 > max)
+{
+    Console.WriteLine($"Число {number3} максимальное");
+}
+else
+{
+    Console.WriteLine($"Число {max} максимальное");
+}
